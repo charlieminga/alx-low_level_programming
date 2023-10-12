c7128ec3c5d4aed4e46f349f8af1c7524f10b0b7
charlieminga <karabo.charlie@gmail.com>
2023-10-12 15:08:13 -0700

    PPP

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,15 @@ int main(void)
 	int digi;
 
 	for (digi = 0; digi <= 9; digi++)
+	{
+	putchar((digi % 10) + '0');
 
-		putchar((digi % 10) + '0')
 		if (digi != 9)
+	{
 		putchar(',');
 		putchar(' ');
-
+	}
+	}
 		putchar('\n');
 		return (0);
 }
