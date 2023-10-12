1330ea43755bf35922677b1347bcafb0a0fbc310
charlieminga <karabo.charlie@gmail.com>
2023-10-12 14:03:56 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Smile in the mirror
+ *
+ *
+ * Return: Alwaays 0 (Success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; alph >= 'z'; alph++)
+
+	putchar(alph);
+	putchar('\n');
+	return (0);
+}
