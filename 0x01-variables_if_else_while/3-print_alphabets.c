fd34c389afe4a4ea49dd27cdeede5daa29466fe1
charlieminga <karabo.charlie@gmail.com>
2023-10-12 03:41:52 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'alphABET'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		putchar(alph);
+	for (alph = 'A'; alph <= 'Z'; alph++)
+		putchar(alph);
+
+	putchar('\n');
+	return (0);
+}
