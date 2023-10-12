1ce2cc24ccd068aa017266f9f5dc271710d7d5e2
charlieminga <karabo.charlie@gmail.com>
2023-10-12 14:57:35 -0700

    PPP

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: PPP
+ *
+ *
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	int digi;
+
+	for (digi = 0; digi <= 9; digi++)
+
+		putchar((digi % 10) + '0')
+		if (digi != 9)
+		putchar(',');
+		putchar(' ');
+
+		putchar('\n');
+		return (0);
+}
