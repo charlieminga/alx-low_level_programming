b6089b7b93e5408a95c6fd86fb6d00b9034e2b29
charlieminga <karabo.charlie@gmail.com>
2023-10-12 02:34:43 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the alphabet game'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph >= 'a' &&)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
